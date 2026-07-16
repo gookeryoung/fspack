ccc61a05884357d39a887374715fe08acc8b62ed
gookeryoung <gooker_young@qq.com>
2026-07-16 17:16:08 +0800

    fix(cli_office): improve pdf file generation error handling

--- examples/cli_office/base_office.py
+++ examples/cli_office/base_office.py
@@ -10,8 +10,14 @@ def main():
     pdf.add_page(page)
     pdf.add_outline_item("示例 - 书签", 1, parent=None)
 
-    with Path("example.pdf").open("wb") as file:
-        pdf.write(file)
+    try:
+        dest_file = Path(__file__).parent / "example.pdf"
+        with dest_file.open("wb") as f:
+            pdf.write(f)
+    except Exception as e:
+        print(f"文件生成失败: {e}")
+    else:
+        print(f"文件生成成功: {dest_file}")
 
 
 if __name__ == "__main__":
